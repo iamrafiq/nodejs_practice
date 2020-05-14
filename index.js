5cd804723e555a47491cc4ff7d6e86d26b6b97c6
linuxrafiq <rafiq.i@live.co.uk>
2020-05-14 15:46:53 +0600

    reading and writting file using call back hell'

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+const sum = (function(a,b){
+    return a+b;
+})(5,6);
 
+console.log(sum);
 
+function add (a, b){
+    return a+b;
+}
+add(5,5);
--- txt/read_write_file.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
-console.log(textIn);
-
-const textOut = `This is what we know about the avacoado ${textIn}\nCreated on ${Date.now()}`;
-fs.writeFileSync('./txt/output.txt', textOut);
-
